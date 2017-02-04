c1857c778b893da21295c84259b39f0c1ade3294
svc37 <shaunculler@hotmail.com>
2017-02-04 02:22:00 -0600

    sql scripts

---
+++ MachineShop/CreateScripts/Stored Procedures/BidEdit.sql
@@ -0,0 +1,37 @@
+use [MachineShop]
+go
+Create procedure [dbo].[EditBid]  
+(  
+	@Id int,
+   @ProjectId int,
+   @SupplierId int,
+   @Price decimal(9,2),  
+   @Time varchar (max), 
+   @QualityLevel varchar (max) = null,
+   --@CreatedDate Date = null,
+   @CreatedBy varchar (max) = null, 
+   @AcceptedDate date = null,
+   @AcceptedBy varchar (max) = null,
+   @Comments varchar (max) = null,
+   @DeclineReason varchar (max) = null,
+   @Declined bit = 0
+  
+)  
+as  
+begin  
+	Update Bids
+	set
+	ProjectId = @ProjectId,
+	SupplierId = @SupplierId,
+    Price = @Price, 
+	Time = @Time,
+	QualityLevel = @QualityLevel, 
+	CreatedBy = @CreatedBy, 
+	AcceptedDate = @AcceptedDate, 
+	AcceptedBy = @AcceptedBy,
+	Comments = @Comments,
+	DeclineReason = @DeclineReason,
+	Declined = @Declined
+
+	where ID = @Id
+End  
